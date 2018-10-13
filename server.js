4354c6fc566ce0bfb6f036bfd85ef17755c2412b
nitinchalopadho <nitin@chalopadho.com>
2018-10-13 13:29:50 +0530

    marks work in progress

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 const app = express();
 app.get('/', function(req, res) {
-    let url = 'http://127.0.0.1:43325/';
+    let url = 'http://127.0.0.1:39349/';
     request(url, function(error, response, html) {
         if (!error) {
             var $ = cheerio.load(html);
@@ -20,6 +20,7 @@ app.get('/', function(req, res) {
             var question_temp = {
                 index: '',
                 content: '',
+                marks: '',
                 parts: [],
             }
             var firstDiv = $('#pf1').text().replace(/\s\s+/gim, '');
@@ -38,8 +39,10 @@ app.get('/', function(req, res) {
             json.date = testDate;
 
             var removeFirstDiv = $('#pf1').nextAll().text();
+            // console.log(removeFirstDiv);
 
-            var removeSpace = removeFirstDiv.replace(/\s\s+/gim, '  ');
+            var removeSpace = removeFirstDiv.replace(/\s\s+/gim, '   ');
+            // console.log(removeSpace);
 
             var removeDots = removeSpace.replace(/\.\.\.*/gim, ' ');
             // console.log(removeDots);
@@ -47,35 +50,47 @@ app.get('/', function(req, res) {
             var addLine = removeDots.replace(/(\D([^+]\d\. |\d\d\. |\d\. |[A-Z]\d\.)|(\d\. ))/gm, '\n$1');
             // console.log(addLine);
 
-            var removeGibberish = addLine.replace(/\[\d\]|\[\d|Option .+/gim, '');
+            var removeGibberish = addLine.replace(/\Option \w .+(?=\[\d\])/gim, '');
             // var removeGibberish = addLine.replace(/\[\d\]|\[\d|Option .+|– \d.+|\(This question .+/gim, '');
             // console.log(removeGibberish);
 
+            var marksList = removeGibberish.match(/\[\d\]/gim);
+            // console.log(marksList);
+
+            var prevMark = removeGibberish.match(/^   \[\d\]/gim);
+            // console.log(firstMark);
+
             var res = removeGibberish.match(/((\d\. |\d\d\. |\d\. |[A-Z]\d\. ).*)/gim);
-            // console.log(res);
+            // console.log(res[0]);
 
             if (res && res.length > 0) {
                 for (var i = 0; i < res.length; i++) {
+                    // console.log(res[i]);
+                    
+
                     question_temp.index = i;
 
+                    // adding a new line before every parts
                     // var subParts = res[i].replace(/(\([a-h]\))/g, '\n$1');
-                    var subParts = res[i].replace(/(  \([a-h]\)  \w|[A-D]\. |[A-D]\. |\(a\) |\(c\) |\(b\) |\(d\) )/gm, '\n$1');
+                    var subParts = res[i].replace(/(  \([a-h]\)  \w|[A-D]\. |[A-D]\. |\(a\) |\(c\) [^t]|\(b\) [^a]|\(d\) |\(e\) |\(f\) |\(h\) )/gm, '\n$1');
                     // console.log(subParts);
 
-                    var removeSubPartsGibberish = subParts.replace(/(– \d.+|\(This question .+)/gm, '');
+                    // removing the gibberish which includes in the subParts
+                    var removeSubPartsGibberish = subParts.replace(/(– \d.+(?=\[\d\]   \[\d\])|– \d\d.+(?=\[\d\]   \[\d\])|– \d.+(?=\[\d\])|\(This question .+(?=\[\d\]   \[\d\])|\(This question .+(?=\[\d\])|– \d.+)/gm, '');
                     // console.log(removeSubPartsGibberish);
 
                     // question_temp.content = subParts.match(/((\d|\d\d|[A-Z]\d)\. .*)/gim)[0];
                     question_temp.content = removeSubPartsGibberish.match(/((\d\. |\d\d\. |\d\. |[A-Z]\d\. ).*)/gim)[0];
 
                     var subPartsArray = [];
-                    subPartsArray = removeSubPartsGibberish.match(/  \([a-h]\)  \w.*|\([a-h]\) \w.*|\([a-h]\)  “\w.*|[A-D]\. .*|[A-D]\. .*/gm);
+                    subPartsArray = removeSubPartsGibberish.match(/  \([a-h]\)  \w.*|\([a-h]\) \w.*|\([a-h]\)   \w.*|\([a-h]\)  “\w.*|[A-D]\. .*|[A-D]\. .*/gm);
                     // console.log(subPartsArray);
 
                     if (subPartsArray && subPartsArray.length > 0) {
                         var question_temp1 = {
                             index: '',
                             content: '',
+                            marks: '',
                             parts: [],
                         }
                         for (var j = 0; j < subPartsArray.length; j++) {
@@ -83,11 +98,12 @@ app.get('/', function(req, res) {
 
                             var nestedSubParts = [];
 
-                            var newI = subPartsArray[j].replace(/\(i\)|\(ii\)|\(iii\)/gim, '\n$&');
+                            var newI = subPartsArray[j].replace(/\(i\)|\(ii\)|\(iii\)/g, '\n$&');
                             // console.log(newI);
 
                             // var ss = newI.match(/(\([a-h]\) .*)/g);
                             var ss = newI.match(/(\([a-f]\) .*|[A-D]\. .*|[A-D]\. .*)/g);
+                            // console.log(ss[0]);
 
                             if (ss == null) {
                                 question_temp1.content = ss;
@@ -95,35 +111,86 @@ app.get('/', function(req, res) {
                                 question_temp1.content = ss[0];
                             }
                             nestedSubParts = newI.match(/\(i\).*|\(ii\).*|\(iii\).*/gim);
+                            
+
                             if (nestedSubParts && nestedSubParts.length > 0) {
                                 var question_temp2 = {
                                     index: '',
                                     content: '',
+                                    marks: '',
                                     parts: [],
                                 }
                                 for (var z = 0; z < nestedSubParts.length; z++) {
                                     question_temp2.index = z;
                                     question_temp2.content = nestedSubParts[z];
+                                    // console.log(z+"--> "+prevMark);
+
+                                    if(prevMark == null){
+                                        prevMark = question_temp1.content.match(/\[\d\]/gim);    
+                                        if(prevMark == null){
+                                            prevMark = question_temp.content.match(/\[\d\]/gim);
+                                            // if(prevMark == null){
+                                            //     prevMark = prevMark[1];
+                                            // }
+                                        }
+                                    }
+                                    var prevMarkList = JSON.stringify(prevMark).split(',');
+                                    
+                                    if(prevMarkList.length>1){
+                                        console.log("before--> "+prevMarkList[0]);
+                                        prevMark = prevMarkList[0].replace(/\["|\"/g, '');
+                                        
+                                    }
+                                    console.log("after--> "+prevMark);
+                                    question_temp2.marks = prevMark;
+                                    prevMark = question_temp2.content.match(/\[\d\]/gim);
+
+                                    // console.log(prevMark);
                                     question_temp1.parts.push(question_temp2);
                                     question_temp2 = {
                                         index: '',
                                         content: '',
+                                        marks: '',
                                         parts: [],
                                     }
                                 }
+                            
                             }
+                            else {
+                                // console.log('topMost2');
+                                // console.log('question_temp1', question_temp1);
+                                if(prevMark == null){
+                                    prevMark = question_temp.content.match(/\[\d\]/gim);    
+                                }
+                                question_temp1.marks = prevMark;
+                                prevMark = question_temp1.content.match(/\[\d\]/gim);
+
+                                // console.log(prevMark);
+                            }
+                            // console.log(question_temp1);
+                            // prevMark = question_temp2.content.match(/\[\d\]/gim);
+
                             question_temp.parts.push(question_temp1);
                             question_temp1 = {
                                 index: '',
                                 content: '',
+                                marks: '',
                                 parts: [],
                             }
                         }
                     }
+                    else{
+                        // console.log('topMost');
+                        question_temp.marks = prevMark;
+                        prevMark = question_temp.content.match(/\[\d\]/gim);
+                        // console.log(prevMark);
+                    }
+                    // console.log(question_temp);
                     json.questions.push(question_temp);
                     question_temp = {
                         index: '',
                         content: '',
+                        marks: '',
                         parts: [],
                     }
                 }
